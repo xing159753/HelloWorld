5f5ee0bf25e978f3a11809593f746ef2c8aa168c
xing159753 <xing159753123@gmail.com>
2022-12-01 15:22:36 +0800

    test

---
+++ TestForLoop.java
@@ -0,0 +1,8 @@
+public class TestForLoop {
+    public static void main(String[] args) {
+        for(int x = 10;x < 20;x++) {
+            System.out.println("x的值是" + x);
+            System.out.println("\n");
+        }
+    }
+}
