f2979ac0ab23a9d4f299f479cc31cb659ad3afe2
xing159753 <xing159753123@gmail.com>
2022-12-06 22:00:28 +0800

    222

--- HomeWork07.java
+++ HomeWork07.java
@@ -1,23 +1,12 @@
 public class HomeWork07 {
     public static void main(String[] args) {
-        int[] arr = {20, -1, 89, 2, 890, 7};
-
-
-        int temp = 0;
-        for(int i = 0; i < arr.length - 1; i++) {
-            for(int j = 0; j < arr.length - 1 -i; j++) {
-                //如果从大到小 arr[j] < arr[j + 1]
-                if(arr[j] > arr[j + 1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
+        //int a = 97;
+        for(char c1 = 'a'; c1 <= 'z'; c1++) {
+            System.out.print(c1 + " ");
         }
-
-        System.out.println("============排序后=================");
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + "\t");
+        System.out.print("\n");
+        for(char c2 = 'Z'; c2 >= 'A'; c2--) {
+            System.out.print(c2 + " ");
         }
     }
 }
