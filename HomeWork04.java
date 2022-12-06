1b1a6a1202889ddafd4343fc833a64ebde39192b
DESKTOP-P2LBU8H\HP <xing159753123@gmail.com>
2022-12-06 17:52:54 +0800

    test

--- HomeWork04.java
+++ HomeWork04.java
@@ -1,20 +1,35 @@
 public class HomeWork04 {
     public static void main(String[] args) {
-//        int a = 1;
-//        int b = 5;
-//        int c = 3;
-//        int num = a * 100 + b * 10 + c;
-//        if( num = a*a*a + b*b*b + c*c*c ) {
-            int n = 153;
-            //n的百位 = n / 100; n的十位 = n % 100 /10; n的个位 = n % 10;
-
-        int n1 = n / 100;
-        int n2 = n % 100 /10;
-        int n3 = n % 10;
-            if(n == n1*n1*n1 + n2*n2*n2 + n3*n3*n3) {
-                System.out.println(n + "是水仙花数");
-        } else {
-                System.out.println(n + "不是水仙花数");
+        int[] arr = {10, 12, 45, 90};
+        int insertNum = 23;
+        int index = -1;
+        for(int i = 0; i < arr.length; i++) {
+            if(insertNum <= arr[i]) {
+                index = i;
+                break;
             }
+        }
+        if(index == -1) {
+            index = arr.length;
+        }
+        //System.out.println("index=" + index);
+        //扩容  先创建一个新的数组 ， 大小 arr.length + 1;
+        int[] arrNew = new int[arr.length + 1];
+        //下面将arr的元素拷贝到 arrNew ,并且要跳过 index 位置；
+         for(int i = 0, j = 0; i < arrNew.length; i++) {
+
+             if(i != index) {
+                 arrNew[i] = arr[j];
+                 j++;
+             } else {
+                 arrNew[i] = insertNum;
+             }
+         }
+         arr = arrNew;
+        System.out.println("==============");
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+
+        }
     }
 }
