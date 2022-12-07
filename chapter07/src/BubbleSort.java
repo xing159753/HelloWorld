28069c581252c56291b6343403029a7bfbea3866
xing159753 <xing159753123@gmail.com>
2022-12-07 17:41:16 +0800

    test

--- chapter07/src/BubbleSort.java
+++ chapter07/src/BubbleSort.java
@@ -1,7 +1,7 @@
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {24, 69, 80, 57, 13, -1, 200, 666, -890};
-        int temp = 0;
+        int temp =  0;
 
         for(int i = 0; i < arr.length - 1; i++) {
             for( int j = 0; j < arr.length - 1 - i; j++) {
