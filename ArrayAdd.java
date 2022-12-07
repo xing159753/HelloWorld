c1107d5e418ba82786afef2f4beaeea600c2636d
xing159753 <xing159753123@gmail.com>
2022-12-07 16:59:19 +0800

    hhhh

--- ArrayAdd.java
+++ ArrayAdd.java
@@ -15,7 +15,7 @@ public class ArrayAdd {
         arr = arrNew;
 
         for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.println(arr[i]) ;
         }
     }
 }
