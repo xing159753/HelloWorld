6781067133f60784af8a08a7ddf13a86d97381ab
xing159753 <xing159753123@gmail.com>
2023-01-24 23:17:47 +0800

    test

---
+++ chapter08/src/com/xing/smallchange/oop/SmallChangeSysApp.java
@@ -0,0 +1,8 @@
+package com.xing.smallchange.oop;
+
+//这里我们直接调用SmallChangeSysOOP对象，显示主菜单即可
+public class SmallChangeSysApp {
+    public static void main(String[] args) {
+        new SmallChangeSysOOP().mainMenu();
+    }
+}
